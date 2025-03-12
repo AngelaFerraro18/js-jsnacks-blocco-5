608185e544a95f75c0cd2087202f62564efa6f31
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-03-12 14:14:27 +0100

    feat: ex 2 completed

--- snack-2/script.js
+++ snack-2/script.js
@@ -6,3 +6,24 @@ const people = [
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
+
+people.forEach((element) => {
+
+  console.log(element.name);
+})
+
+console.log('----');
+
+//versione abbreviata
+
+people.forEach(element => console.log(element.name));
+
+console.log('----');
+
+//versione con il ciclo for
+
+for (let i = 0; i < people.length; i++){
+
+  const person = people[i];
+  console.log(person.name);
+}
