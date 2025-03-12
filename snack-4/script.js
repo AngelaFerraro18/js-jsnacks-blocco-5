2965a4c4805fe735cb194af201fab6f33de6624c
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-03-12 14:31:57 +0100

    feat: ex 4 completed

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,32 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+
+//creo una variabile dove andrò a salvare il valore del nuovo array creato con map
+
+const authorNames = posts.map(element => {
+
+  return element.author;
+})
+
+console.log(authorNames);
+
+//versione ridotta
+
+// const authorNames = posts.map(element => element.author);
+
+// console.log(authorNames);
+
+console.log('-----');
+
+//versione con il ciclo for
+
+let newArray = [];
+for (let i = 0; i < posts.length; i++){
+
+  const post = posts[i];
+  newArray.push(post.author);
+
+}
+console.log(newArray);
