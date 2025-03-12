200684ce39b0f8520aa11931d817e87682fa5025
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-03-12 15:04:09 +0100

    feat: ex 8 completed

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,25 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+
+//creo una variabile dove salvo il valore trovato con find
+
+const classStudent = students.find(element =>{
+  
+  return element.name === 'Marco Lanci';
+})
+console.log(classStudent.class);
+
+console.log('-----');
+
+//versione ciclo for
+
+for (let i = 0; i < students.length; i++){
+
+  const student = students[i];
+
+  if (student.name === 'Marco Lanci'){
+    console.log(student.class);
+  }
+}
