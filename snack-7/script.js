350bc6c5d137261f1334ff649c75fc71b276ec75
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-03-12 14:57:09 +0100

    feat: ex 7 completed

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,25 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+
+//creo una variabile dove salverò il valore recuperato con il metodo find
+
+const student = students.find(element => {
+
+  return element.id === 2;
+
+})
+console.log(student);
+
+console.log('---');
+//versione ciclo for
+
+for (let i = 0; i < students.length; i++){
+
+  const student = students[i];
+  
+  if (student.id === 2){
+    console.log(student);
+  }
+}
