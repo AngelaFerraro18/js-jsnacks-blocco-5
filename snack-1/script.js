fda9808355a7c985dbddd7f00862c3148a03845c
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-03-12 14:08:01 +0100

    feat: ex 1 completed

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,21 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+names.forEach(element => {
+    console.log(element);
+})
+
+console.log('----');
+
+//versione ridotta
+names.forEach(element => console.log(element));
+
+console.log('----');
+
+//versione con il ciclo for
+for (let i = 0; i < names.length; i++){
+
+    const name = names[i];
+    console.log(name);
+}
